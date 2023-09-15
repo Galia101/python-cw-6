3ef1f2dae642981ccb8f9b8de730d61a1b1a8644
Galia101 <galiya.almejrin@gmail.com>
2023-09-15 19:45:48 +0300

    101

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-# write your code here
+person={
+"name": "Galia",
+"age" : 17,
+"hobbie" : ["drawing","gaming"]}
+print(person("name"))
+print(person("age"))
+person("age"):19
+person["country"] = "london"
+print(person)
+print(f"Number of objects in dictionary: {len((person))}")
+person("hobbie").append:["drawing","gaming"]
+def check_hobbies(person):
+    if len(person["hobbies"]) >= 3:
+        return print("WOW YOU ARE AMAZING")
+    else:
+        return print("keep exploring your hobbies!")
+check_hobbies(person)
